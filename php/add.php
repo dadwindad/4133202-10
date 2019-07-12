4610ce9bfa9570461088541cecaa46089e77c84b
dadwindad <vongpramate@gmail.com>
2019-07-12 15:34:41 +0700

    init project

---
+++ php/add.php
@@ -0,0 +1,19 @@
+<?php
+echo $_POST['user'];
+
+echo "Hello World !!!";
+
+$name = "Drusawin";
+echo "Mr." . $name;
+echo "<br>";
+
+echo sum(5,3);
+
+//function
+function sum($A,$B){
+    return $A + $B;
+}
+?>
+
+<!-- short term, use one line only. -->
+<?='My name is Win'?>
